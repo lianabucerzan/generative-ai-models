24ad372b19c1c2561dfdc653dada8c634952cad1
Liana Bucerzan <liana.bucerzan@devhub.tech>
2026-05-27 17:20:53 +0300

    Create prompt and agents for each model

---
+++ backend/agents/index.js
@@ -0,0 +1,4 @@
+export { BaseAgent } from "./BaseAgent.js";
+export { ClaudeAgent } from "./ClaudeAgent.js";
+export { GeminiAgent } from "./GeminiAgent.js";
+export { AgentFactory } from "./AgentFactory.js";
